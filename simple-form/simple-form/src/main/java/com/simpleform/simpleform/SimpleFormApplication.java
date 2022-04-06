08a03f928217d956bc0d7914e58a45d316ec94e5
unknown <roshanrgowda880778@gmail.com>
2022-04-06 08:57:20 +0530

    loginForm

---
+++ simple-form/simple-form/src/main/java/com/simpleform/simpleform/SimpleFormApplication.java
@@ -0,0 +1,14 @@
+package com.simpleform.simpleform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleFormApplication.class, args);
+        System.out.println("hello world");
+	}
+
+}
